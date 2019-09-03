b0687ba3d5d77a33694ab270343c2390cc75a096
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-03 14:44:44 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,10 +6,11 @@ def third_challenge
         :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo"
         },
       :hero_friends => []
    }, 
